cd4886c7851b66cab25e805e7841565f54e6ca1c
vidyasagar <vidyasagar.abburi@yahoo.com>
2023-10-09 13:53:01 +0000

    function

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import Home from './components/Home'
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <ProtectedRoute component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+
+    const response = await fetch(apiUrl, options)
+
+    if (response.ok === true) {
+      const data = await response.json()
+      const fetchedData = data.products.map(each => ({
+        id: each.id,
+        imageUrl: each.image_url,
+        price: each.price,
+        rating: each.rating,
+        title: each.title,
+        brand: each.brand,
+      }))
+      this.setState({productsList: fetchedData})
+    } else {
+      console.error('Failed to fetch data')
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
